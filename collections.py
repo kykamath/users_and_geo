6ae64f1b5edc18ea5365cd0e6f04e68e08b014e4
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-03 18:59:14 -0500

    added collections

---
+++ collections.py
@@ -0,0 +1,22 @@
+'''
+Created on Oct 3, 2011
+
+@author: kykamath
+'''
+from pymongo import Connection, GEO2D, ASCENDING
+
+geoDb = Connection('sid.cs.tamu.edu').geo
+
+'''
+{
+'_id':checkin id, 
+'u': user id, 
+'tw': tweet id, 
+'l': [latitude, longitude], 
+'t': checkin time, 
+'x': text, 
+'lid': location id
+}
+'''
+checkinsCollection = geoDb.checkins
+checkinsCollection.create_index([('uid', ASCENDING), ('l', GEO2D), ('t', ASCENDING), ('lid', ASCENDING)])
--- utilities.py
+++ utilities.py
@@ -3,16 +3,22 @@ Created on Oct 3, 2011
 
 @author: kykamath
 '''
-import cjson
-from library.file_io import FileIO
+import dateutil.parser
+from collections import checkinsCollection
 
 checkins_file = '/mnt/chevron/kykamath/data/geo/checkin_data.txt'
-checkins_json_file = '/mnt/chevron/kykamath/data/geo/checkin_data.json'
 
 def iterateCheckins():
-    for l in open(checkins_file):
-        data = l.split()
-        FileIO.writeToFileAsJson(cjson.encode({'uid': data[0], 'twid': data[1], 'lat': data[2],'lon': data[3], 't': data[4], 'text': data[5], 'pid': data[6]}), checkins_json_file)
+    id = 1
+    for data in open(checkins_file): 
+        data = data.strip().split('\t')
+        if len(data)!=7: data.append(None) 
+        yield {'_id':id, 'u': int(data[0]), 'tw': int(data[1]), 'l': [float(data[2]), float(data[3])], 't': dateutil.parser.parse(data[4]), 'x': data[5], 'lid': data[6]}
+        id+=1
+        if id==10: break
+    
+def addCheckinsToDB():
+    for checkin in iterateCheckins(): checkinsCollection.insert(checkin)
         
 if __name__ == '__main__':
-    iterateCheckins()
+    addCheckinsToDB()
