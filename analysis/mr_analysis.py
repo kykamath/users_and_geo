b9991ae8953f6ca7bbe2361cee915e5a9d96e853
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-04 01:50:34 -0500

    modified mr

--- analysis/mr_analysis.py
+++ analysis/mr_analysis.py
@@ -6,9 +6,17 @@ Created on Oct 4, 2011
 import sys
 sys.path.append('../')
 from analysis.mr_user_distribution import MRUserDistribution
+from analysis.mr_user_to_location_map import MRUserToLocationMap
+
 def userDistribution():
     mrUserDistribution = MRUserDistribution(args='-r hadoop'.split())
     for l in mrUserDistribution.runJob(inputFileList=['hdfs:///user/kykamath/geo/checkin_data.txt']):
         print l
+def userToLocationMap():
+    mrUserToLocationMap = MRUserToLocationMap(args='-r hadoop'.split())
+    for l in mrUserToLocationMap.runJob(inputFileList=['hdfs:///user/kykamath/geo/checkin_data.txt']):
+        print l
+
 if __name__ == '__main__':
-    userDistribution()
+#    userDistribution()
+    userToLocationMap()
--- analysis/mr_user_to_location_map.py
+++ analysis/mr_user_to_location_map.py
@@ -3,14 +3,13 @@ Created on Oct 3, 2011
 
 @author: kykamath
 '''
+import datetime
 from mrjob.job import MRJob
-import dateutil.parser
-import cjson
 
 def parseData(line):
     data = line.strip().split('\t')
     if len(data)!=7: data.append(None) 
-    if len(data)==7: return {'_id':id, 'u': int(data[0]), 'tw': int(data[1]), 'l': [float(data[2]), float(data[3])], 't': dateutil.parser.parse(data[4]), 'x': data[5], 'lid': data[6]}
+    if len(data)==7: return {'_id':id, 'u': int(data[0]), 'tw': int(data[1]), 'l': [float(data[2]), float(data[3])], 't': datetime.datetime.strptime(data[4], '%Y-%m-%d %H:%M:%S'), 'x': data[5], 'lid': data[6]}
 
 class MRUserToLocationMap(MRJob):
     def mapper(self, key, line):
@@ -18,7 +17,7 @@ class MRUserToLocationMap(MRJob):
         if data: yield data['u'], data['lid']
     def reducer(self, word, occurrences):
         occurences = [str(s) for s in occurrences]
-        yield word, '\t'.join(list(set(occurences)))
+        yield word, list(set(occurences))
 
 if __name__ == '__main__':
     MRUserToLocationMap.run()
