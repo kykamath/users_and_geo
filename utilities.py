a96fee868edc291169caaaf730b637cd13c0baa6
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-03 21:50:39 -0500

    modified utilities

--- utilities.py
+++ utilities.py
@@ -10,9 +10,9 @@ checkins_file = '/mnt/chevron/kykamath/data/geo/checkin_data.txt'
 checkin_data_reduced_file = '/mnt/chevron/kykamath/data/geo/checkin_data_reduced.txt'
 
 def iterateCheckins():
-    id = 1
-#    for data in open(checkins_file): 
-    for data in open(checkin_data_reduced_file): 
+    id = checkinsCollection.count()+1
+    for data in open(checkins_file): 
+#    for data in open(checkin_data_reduced_file): 
         data = data.strip().split('\t')
         if len(data)!=7: data.append(None) 
         if len(data) == 7: yield {'_id':id, 'u': int(data[0]), 'tw': int(data[1]), 'l': [float(data[2]), float(data[3])], 't': dateutil.parser.parse(data[4]), 'x': data[5], 'lid': data[6]}
@@ -21,9 +21,7 @@ def iterateCheckins():
 #        if id==1000: break;
     
 def addCheckinsToDB():
-    for checkin in iterateCheckins(): 
-        print checkin
-#        checkinsCollection.insert(checkin)
+    for checkin in iterateCheckins(): checkinsCollection.insert(checkin)
         
 if __name__ == '__main__':
     addCheckinsToDB()
