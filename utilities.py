29cee65e66472d22a57144d47636d4043c9aa5f5
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-03 21:48:47 -0500

    modified utilities

--- utilities.py
+++ utilities.py
@@ -15,13 +15,15 @@ def iterateCheckins():
     for data in open(checkin_data_reduced_file): 
         data = data.strip().split('\t')
         if len(data)!=7: data.append(None) 
-        if len(data) == 7: print {'_id':id, 'u': int(data[0]), 'tw': int(data[1]), 'l': [float(data[2]), float(data[3])], 't': dateutil.parser.parse(data[4]), 'x': data[5], 'lid': data[6]}
+        if len(data) == 7: yield {'_id':id, 'u': int(data[0]), 'tw': int(data[1]), 'l': [float(data[2]), float(data[3])], 't': dateutil.parser.parse(data[4]), 'x': data[5], 'lid': data[6]}
         else: print 'Problem in line: ', id, data
         id+=1
 #        if id==1000: break;
     
 def addCheckinsToDB():
-    for checkin in iterateCheckins(): checkinsCollection.insert(checkin)
+    for checkin in iterateCheckins(): 
+        print checkin
+#        checkinsCollection.insert(checkin)
         
 if __name__ == '__main__':
     addCheckinsToDB()
