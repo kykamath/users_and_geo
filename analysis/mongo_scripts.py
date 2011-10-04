7dd22323b26da98abe047cfcff83c55064eb7185
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-04 12:26:11 -0500

    modified analysis

--- analysis/mongo_scripts.py
+++ analysis/mongo_scripts.py
@@ -11,14 +11,13 @@ from mongo_settings import checkinsCollection, venuesCollection
 from library.geo import getLidFromLocation
 
 def addCheckinsToDB():
-#    i = 0
+    i = 0
     for data in open(checkinsFile):
         data = data.strip().split('\t')
-        if len(data)!=7: data.append(None) 
-        if len(data) == 7: checkinsCollection.insert({'_id':int(data[1]), 'u': int(data[0]), 'l': [float(data[2]), float(data[3])], 'lid': getLidFromLocation([float(data[2]), float(data[3])]), 't': dateutil.parser.parse(data[4]), 'x': data[5], 'pid': data[6]})
-        else: print 'Problem in line: ', data
-#        i+=1
-#        if i==1000: break;
+        try:
+            if len(data)!=7: data.append(None) 
+            if len(data) == 7: checkinsCollection.insert({'_id':int(data[1]), 'u': int(data[0]), 'l': [float(data[2]), float(data[3])], 'lid': getLidFromLocation([float(data[2]), float(data[3])]), 't': dateutil.parser.parse(data[4]), 'x': data[5], 'pid': data[6]})
+        except Exception as e: print i, 'Exception while processing:', data; i+=1
 
 def addVenuesToDB():
     i = 0
@@ -27,9 +26,7 @@ def addVenuesToDB():
         try:
             venuesCollection.insert({'_id': int(data[0]), 'n': data[1], 'l': [float(data[2]), float(data[3])], 'lid': getLidFromLocation([float(data[2]), float(data[3])]), 'm':' '.join(data[4:-2]), 'tp': int(data[-2]),  'tc': int(data[-1])})
         except Exception as e: print i, 'Exception while processing:', data; i+=1
-#        i+=1
-#        if i==1000: break;
 
 if __name__ == '__main__':
-#    addCheckinsToDB()
-    addVenuesToDB()
+    addCheckinsToDB()
+#    addVenuesToDB()
