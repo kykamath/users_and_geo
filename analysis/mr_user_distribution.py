66d75c2752257d0ed9ae4c2a7b600f7146982615
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-04 01:37:22 -0500

    modified mr

--- analysis/mr_user_distribution.py
+++ analysis/mr_user_distribution.py
@@ -3,13 +3,13 @@ Created on Oct 3, 2011
 
 @author: kykamath
 '''
-import dateutil.parser
+import datetime
 from library.mrjobwrapper import ModifiedMRJob
 
 def parseData(line):
     data = line.strip().split('\t')
     if len(data)!=7: data.append(None) 
-    if len(data)==7: return {'_id':id, 'u': int(data[0]), 'tw': int(data[1]), 'l': [float(data[2]), float(data[3])], 't': dateutil.parser.parse(data[4]), 'x': data[5], 'lid': data[6]}
+    if len(data)==7: return {'_id':id, 'u': int(data[0]), 'tw': int(data[1]), 'l': [float(data[2]), float(data[3])], 't': datetime.datetime.strptime(data[4], '%Y-%m-%d %H:%M:%S'), 'x': data[5], 'lid': data[6]}
 
 class MRUserDistribution(ModifiedMRJob):
     DEFAULT_INPUT_PROTOCOL='raw_value'
