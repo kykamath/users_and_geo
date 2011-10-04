abc71a48bc70758d5ab59d89c47221edb6645ac1
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-04 01:52:28 -0500

    modified mr

--- analysis/mr_user_to_location_map.py
+++ analysis/mr_user_to_location_map.py
@@ -4,14 +4,15 @@ Created on Oct 3, 2011
 @author: kykamath
 '''
 import datetime
-from mrjob.job import MRJob
+from library.mrjobwrapper import ModifiedMRJob
 
 def parseData(line):
     data = line.strip().split('\t')
     if len(data)!=7: data.append(None) 
     if len(data)==7: return {'_id':id, 'u': int(data[0]), 'tw': int(data[1]), 'l': [float(data[2]), float(data[3])], 't': datetime.datetime.strptime(data[4], '%Y-%m-%d %H:%M:%S'), 'x': data[5], 'lid': data[6]}
 
-class MRUserToLocationMap(MRJob):
+class MRUserToLocationMap(ModifiedMRJob):
+    DEFAULT_INPUT_PROTOCOL='raw_value'
     def mapper(self, key, line):
         data = parseData(line)
         if data: yield data['u'], data['lid']
