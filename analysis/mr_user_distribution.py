61712e4592d7ad9365e0356d23c6751fe7dcc2ec
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-04 09:39:32 -0500

    modified analysis

--- analysis/mr_user_distribution.py
+++ analysis/mr_user_distribution.py
@@ -3,13 +3,8 @@ Created on Oct 3, 2011
 
 @author: kykamath
 '''
-import datetime
 from library.mrjobwrapper import ModifiedMRJob
-
-def parseData(line):
-    data = line.strip().split('\t')
-    if len(data)!=7: data.append(None) 
-    if len(data)==7: return {'_id':id, 'u': int(data[0]), 'tw': int(data[1]), 'l': [float(data[2]), float(data[3])], 't': datetime.datetime.strptime(data[4], '%Y-%m-%d %H:%M:%S'), 'x': data[5], 'lid': data[6]}
+from library.geo import parseData
 
 class MRUserDistribution(ModifiedMRJob):
     DEFAULT_INPUT_PROTOCOL='raw_value'
