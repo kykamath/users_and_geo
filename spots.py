658b263868f0d9bab726b6d0605be83cf54abc6d
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-04 22:20:06 -0500

    modified analysis

--- spots.py
+++ spots.py
@@ -63,8 +63,9 @@ if __name__ == '__main__':
     # of the map.
     # resolution = 'i' means use intermediate resolution coastlines.
     # lon_0, lat_0 are the central longitude and latitude of the projection.
-    m = Basemap(llcrnrlon=-10.5,llcrnrlat=49.5,urcrnrlon=3.5,urcrnrlat=59.5,
-                resolution='i',projection='tmerc',lon_0=-4.36,lat_0=54.7)
+#    m = Basemap(llcrnrlon=-10.5,llcrnrlat=49.5,urcrnrlon=3.5,urcrnrlat=59.5,
+#                resolution='i',projection='tmerc',lon_0=-4.36,lat_0=54.7)
+    m = Basemap(projection='robin',lon_0=0,resolution='c')
     # can get the identical map this way (by specifying width and
     # height instead of lat/lon corners)
     #m = Basemap(width=894887,height=1116766,\
