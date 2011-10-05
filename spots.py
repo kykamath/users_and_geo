4a1f1bacf272dcc786f30b1fdb72e77068979c1a
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-04 22:33:34 -0500

    modified analysis

--- spots.py
+++ spots.py
@@ -20,7 +20,11 @@ class Map:
     @staticmethod
     def onWorldMapPlot(dataTuplesToPlot):
         from mpl_toolkits.basemap import Basemap
+#        m = Basemap(projection='robin',lon_0=0,resolution='c')
         m = Basemap(projection='robin',lon_0=0,resolution='c')
+        m.drawcoastlines()
+        m.fillcontinents(color='#FFFFFF',lake_color='#FFFFFF')
+        m.drawmapboundary(fill_color='#FFFFFF') 
         for longitudes, latitudes, color in dataTuplesToPlot:
             x,y = m(longitudes,latitudes)
             m.plot(x,y,color=color, lw=0, marker='o')
@@ -48,33 +52,9 @@ def cluster():
     plt.savefig('worldmap.pdf')
     
 if __name__ == '__main__':
-#    Map.onWorldMapPlot([
-#                        ([-105.16, -117.16, -77.00], [40.02, 32.73, 38.55], GeneralMethods.getRandomColor()),
-#                        ([-114.21, -88.10], [48.25, 17.29], GeneralMethods.getRandomColor())
-#                        ])
-#    plt.show()
-#    cluster()
-
-    from mpl_toolkits.basemap import Basemap
-    import numpy as np
-    import matplotlib.pyplot as plt
-    # llcrnrlat,llcrnrlon,urcrnrlat,urcrnrlon
-    # are the lat/lon values of the lower left and upper right corners
-    # of the map.
-    # resolution = 'i' means use intermediate resolution coastlines.
-    # lon_0, lat_0 are the central longitude and latitude of the projection.
-#    m = Basemap(llcrnrlon=-10.5,llcrnrlat=49.5,urcrnrlon=3.5,urcrnrlat=59.5,
-#                resolution='i',projection='tmerc',lon_0=-4.36,lat_0=54.7)
-    m = Basemap(projection='robin',lon_0=0,resolution='c')
-    # can get the identical map this way (by specifying width and
-    # height instead of lat/lon corners)
-    #m = Basemap(width=894887,height=1116766,\
-    #            resolution='i',projection='tmerc',lon_0=-4.36,lat_0=54.7)
-    m.drawcoastlines()
-    m.fillcontinents(color='#FFFFFF',lake_color='#FFFFFF')
-    # draw parallels and meridians.
-#    m.drawparallels(np.arange(-40,61.,2.))
-#    m.drawmeridians(np.arange(-20.,21.,2.))
-    m.drawmapboundary(fill_color='#FFFFFF') 
-    plt.title("Transverse Mercator Projection")
+    Map.onWorldMapPlot([
+                        ([-105.16, -117.16, -77.00], [40.02, 32.73, 38.55], GeneralMethods.getRandomColor()),
+                        ([-114.21, -88.10], [48.25, 17.29], GeneralMethods.getRandomColor())
+                        ])
     plt.savefig('map.png')
+#    cluster()
