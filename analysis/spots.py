2d909b14c0d42da6f50b882545968cadafcd153b
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-05 19:15:29 -0500

    added code for kml

--- analysis/spots.py
+++ analysis/spots.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 from library.classes import GeneralMethods
 from library.geo import geographicConvexHull
-from settings import us_boundary, radiusSpotsFolder, radiusSpotsKmlsFolder
+from settings import radiusSpotsFolder, radiusSpotsKmlsFolder
 from library.plotting import Map, getDataDistribution
 
 class SpotsKML:
@@ -33,37 +33,37 @@ class SpotsKML:
 
 def nearbyLocations(lid, radiusInMiles): return (location for location in locationsCollection.find({"l": {"$within": {"$center": [getLocationFromLid(lid), convertMilesToRadians(radiusInMiles)]}}}))
 
-def cluster():
-    i = 0
-    radiusInMiles = 5
-    graph = nx.Graph()
-    longs, lats = [], []
-    for lid in locationIterator():
-        if isWithinBoundingBox(getLocationFromLid(lid), us_boundary):
-            print i
-            for location in nearbyLocations(lid, radiusInMiles): graph.add_edge(location['_id'], lid)
-            i+=1
-            if i==50000: break
-    i = 0
-    clustersData = []
-    for component in nx.connected_components(graph):
-#        if len(component)>=5: 
-        i+=1
-        longitudes, latitudes = zip(*[getLocationFromLid(l) for l in component])
-        longs+=longitudes; lats+=latitudes
-#        if i==5: break;
-        clustersData.append((list(longitudes), list(latitudes), GeneralMethods.getRandomColor()))
-    print len(nx.connected_components(graph))
-    usMap = Map()
-    for lats, longs, color in clustersData: usMap.plotPoints(lats, longs, color)
-    plt.savefig('worldmap.png')
+#def cluster():
+#    i = 0
+#    radiusInMiles = 5
+#    graph = nx.Graph()
+#    longs, lats = [], []
+#    for lid in locationIterator():
+#        if isWithinBoundingBox(getLocationFromLid(lid), us_boundary):
+#            print i
+#            for location in nearbyLocations(lid, radiusInMiles): graph.add_edge(location['_id'], lid)
+#            i+=1
+#            if i==50000: break
+#    i = 0
+#    clustersData = []
+#    for component in nx.connected_components(graph):
+##        if len(component)>=5: 
+#        i+=1
+#        longitudes, latitudes = zip(*[getLocationFromLid(l) for l in component])
+#        longs+=longitudes; lats+=latitudes
+##        if i==5: break;
+#        clustersData.append((list(longitudes), list(latitudes), GeneralMethods.getRandomColor()))
+#    print len(nx.connected_components(graph))
+#    usMap = Map()
+#    for lats, longs, color in clustersData: usMap.plotPoints(lats, longs, color)
+#    plt.savefig('worldmap.png')
 
 def generateRadiusSpots(radiusInMiles):
     graph = nx.Graph()
     spotsFile = radiusSpotsFolder+'%s'%(radiusInMiles)
     for lid in locationIterator():
-        if isWithinBoundingBox(getLocationFromLid(lid), us_boundary):
-            for location in nearbyLocations(lid, radiusInMiles): graph.add_edge(location['_id'], lid)
+#        if isWithinBoundingBox(getLocationFromLid(lid), us_boundary):
+        for location in nearbyLocations(lid, radiusInMiles): graph.add_edge(location['_id'], lid)
     for venues in nx.connected_components(graph): FileIO.writeToFileAsJson({'venues': venues}, spotsFile)
 def generateStatsForRadiusSpots(): [generateRadiusSpots(radius) for radius in [1,5,10,15,20]]
 def radiusSpotsIterator(radiusInMiles, minVenues=0): return (data['venues'] for data in FileIO.iterateJsonFromFile(radiusSpotsFolder+'%s'%(radiusInMiles)) if len(data['venues'])>=minVenues)
@@ -103,7 +103,7 @@ def generateKMLsRadiusSpots(): [generateRadiusSpots(radius) for radius in [1,5,1
 
     
 if __name__ == '__main__':
-#    generateStatsForRadiusSpots()
+    generateStatsForRadiusSpots()
 #    plotRadiusSpotDistribution()
 #    plotRadiusSpots()
-    drawKMLsForRadiusSpots()
+#    drawKMLsForRadiusSpots()
