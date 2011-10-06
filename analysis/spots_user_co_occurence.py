23f2f6d2ca66eacb47c3df9293fd9bf2c7401486
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-05 23:58:02 -0500

    modified analysis

--- analysis/spots_user_co_occurence.py
+++ analysis/spots_user_co_occurence.py
@@ -4,26 +4,24 @@ Created on Oct 5, 2011
 @author: kykamath
 '''
 import sys
+from library.file_io import FileIO
 sys.path.append('../')
 from analysis import SpotsKML
-from settings import userCooccurenceKmlsFolder
+from settings import userCooccurenceKmlsFolder, locationGraph
 from library.geo import getLocationFromLid
-import cjson
 
 def getLocationPairs(edge): 
     data = edge.split()
     return [getLocationFromLid(data[0]+' '+data[1]), getLocationFromLid(data[2]+' '+data[3])]
     
     
-def drawKMLsForUserCooccurenceSpots():
+def drawKMLsForUserCooccurenceSpots(minEdgeWeight=30):
     kml = SpotsKML()
-#    for locations in radiusSpotsIterator(radius, minLocations): kml.addPoints(locations)
-    for edge in [cjson.decode('{"e": "63.299 28.050 63.290 28.025", "w": 1}')]:
-        print edge
-#        print getLocationPairs(edge['e'])
-        kml.addLine(getLocationPairs(edge['e']), description=str(edge['w']))
-#        kml.addLine(getLocationPairs(edge['e']))
-    kml.write(userCooccurenceKmlsFolder+'user_co_occurence.kml')
+    i=1
+    for edge in FileIO.iterateJsonFromFile(locationGraph):
+        if edge['w']>=minEdgeWeight: kml.addLine(getLocationPairs(edge['e']), description=str(edge['w'])); i+=1
+        if i==10000: break
+    kml.write(userCooccurenceKmlsFolder+'%s.kml'%minEdgeWeight)
 
 if __name__ == '__main__':
     drawKMLsForUserCooccurenceSpots()
