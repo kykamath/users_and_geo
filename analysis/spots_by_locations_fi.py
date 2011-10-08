3e16a96c5b69844b5680ca47cbb7f92c8a14c1bc
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-07 21:12:50 -0500

    modified experiments

--- analysis/spots_by_locations_fi.py
+++ analysis/spots_by_locations_fi.py
@@ -27,7 +27,7 @@ def locationTransactionsIterator():
             d[k]-=1
             if d[k]==0: del d[k]
     
-    for d in userToLocationMapIterator(minLocations=150):
+    for d in userToLocationMapIterator(minLocations=50):
         while len(d.keys())>=minimumTransactionLength: 
             yield d.keys()
             decrementDictionary(d)
@@ -83,8 +83,8 @@ def drawKMLsForUserBasedSpotsUsingFI(minSupport=minSupport, minLocations=6):
     
     
 if __name__ == '__main__':
-#    writeInputFileForFIMahout()
-    calculateFrequentLocationItemsets()
+    writeInputFileForFIMahout()
+#    calculateFrequentLocationItemsets()
 #    getMahoutOutput()
 #    drawKMLsForUserBasedSpotsUsingFI(minSupport=10)
 #    analyzeFrequentLocations()
