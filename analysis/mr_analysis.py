11f2ca5137192e9b401b3a1d0e864c3d399bdd06
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-08 12:09:38 -0500

    modifed analysis

--- analysis/mr_analysis.py
+++ analysis/mr_analysis.py
@@ -71,6 +71,6 @@ if __name__ == '__main__':
 #    print len(list(locationByUserDistributionIterator(minTimesUserCheckedIn=10)))
     i = 0
     for u in filteredUserIterator(10, 0):
-        print u['user']
+        print u
         i+=1
         if i>5: exit()
