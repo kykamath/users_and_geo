6b4459a17d132c2fb00b0cfbaf264b3b5d899c5f
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-11 23:40:14 -0400

    modified analysis

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -75,5 +75,5 @@ class SpotsFile():
             for location, _ in spot['spot']: lidToSpotIdMap[getLidFromLocation(location)] = spot['id']
         for userVector in userToLocationVector: 
             spotDistribution = defaultdict(int)
-            for lid in userVector: spotDistribution[lidToSpotIdMap]+=1
+            for lid in userVector: spotDistribution[lidToSpotIdMap[lid]]+=1
             print spotDistribution
