95bb3352545c474981454e1ed45530f381685e5c
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-13 19:32:47 -0500

    modified analysis

--- analysis/mr_location_user_spots.py
+++ analysis/mr_location_user_spots.py
@@ -5,22 +5,36 @@ Created on Oct 13, 2011
 '''
 import sys
 sys.path.append('../')
+from multiprocessing import Pool
 from settings import minLocationsTheUserHasCheckedin,\
     minUniqueUsersCheckedInTheLocation, minimumLocationsPerSpot
 from mongo_settings import locationsCollection
-from library.geo import getLocationFromLid, convertMilesToRadians
+from library.geo import getLocationFromLid, convertMilesToRadians,\
+    getHaversineDistanceForLids
 import networkx as nx
-from analysis.mr_analysis import getfilteredLocationsSet
-    
-def iterateSpotsUsingRadius(minLocationsTheUserHasCheckedin, minUniqueUsersCheckedInTheLocation, radius):
-    def nearbyLocations(lid, radiusInMiles): return (location for location in locationsCollection.find({"l": {"$within": {"$center": [getLocationFromLid(lid), convertMilesToRadians(radiusInMiles)]}}}))
+from analysis.mr_analysis import getfilteredLocationsSet,\
+    locationByUserDistributionIterator
+from itertools import combinations
+
+
+radiusInMiles = 0.5
+
+def f((x1,x2)): 
+    d = getHaversineDistanceForLids(x1,x2)
+    if d<=radiusInMiles: return (x1,x2,d)
+def iterateSpotsUsingRadius(minLocationsTheUserHasCheckedin, minUniqueUsersCheckedInTheLocation):
+    p = Pool()
+    i = 1
     graph = nx.Graph()
-    locationSet = getfilteredLocationsSet(minLocationsTheUserHasCheckedin, minUniqueUsersCheckedInTheLocation)
-    for lid in locationSet:
-        for location in nearbyLocations(lid, radius): 
-            if location['_id'] in locationSet: graph.add_edge(location['_id'], lid)
+    for d in p.imap(f, combinations(locationByUserDistributionIterator(minUniqueUsersCheckedInTheLocation),2)):
+        if d:
+            l1, l2, _ = d
+            graph.add_edge(l1, l2)
+            print i, l1, l2
+        i+=1
+        if i==200000: break
     for locations in nx.connected_components(graph): 
         if len(locations)>=minimumLocationsPerSpot: print locations
-        
+
 if __name__ == '__main__':
-    iterateSpotsUsingRadius(minLocationsTheUserHasCheckedin, minUniqueUsersCheckedInTheLocation, radius=1)
+    iterateSpotsUsingRadius(minLocationsTheUserHasCheckedin, minUniqueUsersCheckedInTheLocation)
