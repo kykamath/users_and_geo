ea4e15766ccb15d659b38b742a87c0946d1d4eb8
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-14 00:28:57 -0500

    modified analysis

--- analysis/mr_location_user_spots.py
+++ analysis/mr_location_user_spots.py
@@ -56,7 +56,7 @@ class UserGraphSpots:
         graph = nx.Graph()
         for e in locationToLocationCollection.find():
             d = e['_id'].split()
-            l1, l2 = ' '.join(d[:2]), ' '.join(d[2:])
+            l1, l2 = '_'.join(d[:2]), '_'.join(d[2:])
             if l1 in locationsToCheck and l2 in locationsToCheck and e['d']<=graphNodesDistanceInMiles and e['u']>=graphNodesMinEdgeWeight: graph.add_edge(l1, l2)
         for locations in nx.connected_components(graph): 
             if len(locations)>=minimumLocationsPerSpot: 
