31576b2341c87d8cc9fe646aea53c120334ec6b8
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-15 20:12:03 -0500

    modified analysis

--- analysis/user_clustering.py
+++ analysis/user_clustering.py
@@ -5,12 +5,13 @@ Created on Oct 15, 2011
 '''
 import sys
 sys.path.append('../')
-from analysis.mr_location_user_dayblock_mad import getLocationUserSpecificMads
 from analysis.mr_analysis import filteredUserIterator,\
     filteredLocationToUserAndTimeMapIterator, locationsForUsIterator
 from settings import minLocationsTheUserHasCheckedin,\
     minUniqueUsersCheckedInTheLocation
 from operator import itemgetter
+from library.plotting import getDataDistribution
+
 def getUserVectors():
     ''' Returns a dict for user vectors across top 100 location dimensions.
     '''
@@ -18,7 +19,14 @@ def getUserVectors():
 #userVectors = getUserVectors()
 
 def getDayBlockDistributionForUsers(locationVector):
-    print getLocationUserSpecificMads(locationVector)
+    completeDayBlockDistribution = []
+    for user in locationVector['users']:
+        dayBlockDistributionForUser = []
+        for day in locationVector['users'][user]:
+            dayBlockDistributionForUser+=[int(dayBlock) for dayBlock in locationVector['users'][user][day] for i in range(locationVector['users'][user][day][dayBlock])]
+        completeDayBlockDistribution+=dayBlockDistributionForUser
+    dataX, dataY = getDataDistribution(completeDayBlockDistribution)
+    print dict((str(x),y) for x,y in zip(dataX, dataY))
 
 locationsInUS = set(list(locationsForUsIterator(minUniqueUsersCheckedInTheLocation)))
 for location in filter(lambda l: l['location'] in locationsInUS, filteredLocationToUserAndTimeMapIterator(minLocationsTheUserHasCheckedin, minUniqueUsersCheckedInTheLocation)): 
