f8b38b9376c3f0c0160110fcebd07640e8ad474a
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-15 20:42:15 -0500

    modified analysis

--- analysis/user_clustering.py
+++ analysis/user_clustering.py
@@ -10,6 +10,7 @@ from analysis.mr_analysis import filteredUserIterator,\
 from settings import minLocationsTheUserHasCheckedin,\
     minUniqueUsersCheckedInTheLocation
 from operator import itemgetter
+from collections import defaultdict
 import numpy as np
 import random
 
@@ -19,17 +20,17 @@ def getUserVectors():
     return dict((u['user'], dict(sorted(u['locations'].iteritems(), key=itemgetter(1), reverse=True)[:100])) for u in filteredUserIterator(minLocationsTheUserHasCheckedin, minUniqueUsersCheckedInTheLocation,  fullRecord = True))
 
 #userVectors = getUserVectors()
-def getDayBlockDistributionForUsers(users, userClusters):
-    completeDayBlockDistribution = []
+def getDayBlockDistributionForUsers(users, userClusterMap):
+    completeDayBlockDistribution = defaultdict(list)
     for user in users:
         dayBlockDistributionForUser = []
         for day in users[user]:
             dayBlockDistributionForUser+=[int(dayBlock) for dayBlock in users[user][day] for i in range(users[user][day][dayBlock])]
-        completeDayBlockDistribution+=dayBlockDistributionForUser
-    print np.mean(completeDayBlockDistribution), completeDayBlockDistribution
-
+        completeDayBlockDistribution[userClusterMap[user]]+=dayBlockDistributionForUser
+    print [(k, np.mean(completeDayBlockDistribution)) for k in completeDayBlockDistribution]
+    
 locationsInUS = set(list(locationsForUsIterator(minUniqueUsersCheckedInTheLocation)))
 for location in filter(lambda l: l['location'] in locationsInUS, filteredLocationToUserAndTimeMapIterator(minLocationsTheUserHasCheckedin, minUniqueUsersCheckedInTheLocation)): 
-#    getDayBlockDistributionForUsers('location['users']')
-    print dict((u, random.randint(0,2)) for u in location['users'])
+    userClusterMap = dict((u, random.randint(0,2)) for u in location['users'])
+    getDayBlockDistributionForUsers(location['users'], userClusterMap)
     
