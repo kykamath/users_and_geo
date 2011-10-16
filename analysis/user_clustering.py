b9a66a16db6bf18e2f7675a813b45206c6633459
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-16 00:53:06 -0500

    modified analysis

--- analysis/user_clustering.py
+++ analysis/user_clustering.py
@@ -47,7 +47,7 @@ def clusterLocation(location):
         cluster = KMeansClustering(userVectorsToCluster, k).cluster()
         userClusterMap = dict((k1,v) for k1,v in zip(location['users'], cluster))
         dayBlockMeansForClusters = getDayBlockMeansForClusters(location['users'], userClusterMap)
-        userClusterMap = dict([(str(k), v) for k, v in userClusterMap.iteritems()])
+        userClusterMap = dict([(str(k2), v) for k2, v in userClusterMap.iteritems()])
         resultsForVaryingK.append([k, userClusterMap, zip(*dayBlockMeansForClusters)[1:], getAverageDistanceBetweenClusters(zip(*dayBlockMeansForClusters)[1])])
     return sorted(resultsForVaryingK, key=itemgetter(3))[-1]
 
