9902d99ea9c925848890c169ebdae5fd31707ad7
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-25 18:10:36 -0500

    modified analysis

--- analysis/places.py
+++ analysis/places.py
@@ -75,7 +75,7 @@ def writeUserClusters(place):
     for user in userVectors.keys()[:]: 
         if sum(userVectors[user].itervalues())<place['minUserCheckins']: del userVectors[user]
     resultsForVaryingK = []
-    for k in range(4, 10):
+    for k in range(4, 20):
         try:
             print 'Clustering with k=%s'%k
             clusters = KMeansClustering(userVectors.iteritems(), k, documentsAsDict=True).cluster(normalise=True, assignAndReturnDetails=True, repeats=5, numberOfTopFeatures=numberOfTopFeatures, algorithmSource='biopython')
@@ -355,7 +355,7 @@ place = {'name':'austin_tx', 'boundary':austin_tx_boundary, 'minUserCheckins':5,
 
 #writeLocationToUserMap(place)
 writeUserClusters(place)
-#getUserClusterDetails(place)
+getUserClusterDetails(place)
 
 #writeLocationsWithClusterInfoFile(place)
 #writeLocationClusters(place)
