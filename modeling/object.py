8f0fca39275c16a4af1de010d4b2faac63edfe08
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-28 00:55:10 -0500

    modified analysis

---
+++ modeling/object.py
@@ -0,0 +1,15 @@
+'''
+Created on Oct 27, 2011
+
+@author: kykamath
+'''
+class User:
+    def __init__(self):
+        self.locations = []
+
+class Location:
+    pass
+
+class Advertiser:
+    def __init__(self):
+        self.interestedLocations
