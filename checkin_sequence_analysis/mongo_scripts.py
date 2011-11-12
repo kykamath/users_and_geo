be2efdec03e86b4df874478b9598a95ad7b3ce7a
Krishna Kamath <krishna.kamath@gmail.com>
2011-11-11 23:40:30 -0600

    modifie sequence analysis

--- checkin_sequence_analysis/mongo_scripts.py
+++ checkin_sequence_analysis/mongo_scripts.py
@@ -5,12 +5,15 @@ Created on Nov 11, 2011
 '''
 import sys
 sys.path.append('../')
+from mongo_settings import checkinsCollection
 from analysis.mr_analysis import filteredUserIterator
 from settings import minLocationsTheUserHasCheckedin,\
     minUniqueUsersCheckedInTheLocation
     
-#userSet = set([userVector['user'] for userVector in filteredUserIterator(minLocationsTheUserHasCheckedin, minUniqueUsersCheckedInTheLocation, fullRecord = True)])
-#for user in userSet:
-#    print user
-for userVector in filteredUserIterator(minLocationsTheUserHasCheckedin, minUniqueUsersCheckedInTheLocation, fullRecord = True):
-    print userVector
+userSet = set([userVector['user'] for userVector in filteredUserIterator(minLocationsTheUserHasCheckedin, minUniqueUsersCheckedInTheLocation, fullRecord = True)])
+for user in userSet:
+    print user
+    for c in checkinsCollection.find({'u': user}): print c
+    exit()
+#for userVector in filteredUserIterator(minLocationsTheUserHasCheckedin, minUniqueUsersCheckedInTheLocation, fullRecord = True):
+#    print userVector
