ff57c0bf9033ab275970d44579c33196f85e4f09
Krishna Kamath <krishna.kamath@gmail.com>
2011-11-12 00:08:08 -0600

    modifie sequence analysis

--- checkin_sequence_analysis/mongo_scripts.py
+++ checkin_sequence_analysis/mongo_scripts.py
@@ -14,8 +14,8 @@ from settings import minLocationsTheUserHasCheckedin,\
 userSet = set([userVector['user'] for userVector in filteredUserIterator(minLocationsTheUserHasCheckedin, minUniqueUsersCheckedInTheLocation, fullRecord = True)])
 for user in userSet:
     print user
-    checkins = [(c['_id'], c['lid'], c['t'].epoch) for c in checkinsCollection.find({'u': user})]
-    for i in GeneralMethods.getElementsInWindow(checkins, 2):
+    checkins = [(c['_id'], c['lid'], c['t'], c['t'].timetuple()) for c in checkinsCollection.find({'u': user})]
+    for i in GeneralMethods.getElementsInWindow(checkins, 1):
         print i
     exit()
 #for userVector in filteredUserIterator(minLocationsTheUserHasCheckedin, minUniqueUsersCheckedInTheLocation, fullRecord = True):
