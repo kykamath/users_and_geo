3f4ef127db54d064c735d2b3aa7a9f466e84c565
Krishna Kamath <krishna.kamath@gmail.com>
2011-11-12 22:34:57 -0600

    modifie sequence analysis

--- checkin_sequence_analysis/sequences.py
+++ checkin_sequence_analysis/sequences.py
@@ -31,8 +31,9 @@ def createLocationFile():
 #        print location['_id'], unicode(location['n']).encode('utf-8')
         print location.keys()
         lid = location['_id']
-        for edge in location['_id']['e'][OUTGOING_EDGE]:
-            print edge
+        print location['_id']['e'].keys()
+#        for edge in location['_id']['e'][OUTGOING_EDGE]:
+#            print edge
         
         exit()
 
