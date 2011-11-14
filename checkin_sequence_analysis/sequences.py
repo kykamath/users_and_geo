3f304c7d5a5887a93ce921e63b0fa5fb12b0f8d8
Krishna Kamath <krishna.kamath@gmail.com>
2011-11-14 17:22:57 -0600

    minor modifications

--- checkin_sequence_analysis/sequences.py
+++ checkin_sequence_analysis/sequences.py
@@ -159,7 +159,7 @@ class NeighboringLocationsAnalysis():
     @staticmethod
     def generateData():
         for i in range(1,11):
-            regex = 'cafe'
+            regex = 'starbuck'
 #            NeighboringLocationsAnalysis.analyze(regex, NeighborLocationsSelection.N_PREVIOUS_LOCATIONS, minEdgeWeightInNRGraph=3, checkinsWindow=i)
 #            NeighboringLocationsAnalysis.analyze(regex, NeighborLocationsSelection.N_FUTURE_LOCATIONS, minEdgeWeightInNRGraph=3, checkinsWindow=i)
             NeighboringLocationsAnalysis.analyze(regex, NeighborLocationsSelection.N_LOCATIONS, minEdgeWeightInNRGraph=10, checkinsWindow=i)
@@ -197,7 +197,7 @@ if __name__ == '__main__':
 #    writeCheckinSequenceGraphFile()
 #    createLocationFile(regex='starbuck')
     
-#    NeighboringLocationsAnalysis.generateData()
-    NeighboringLocationsAnalysis.analyzeDataClusters()
+    NeighboringLocationsAnalysis.generateData()
+#    NeighboringLocationsAnalysis.analyzeDataClusters()
     
 
