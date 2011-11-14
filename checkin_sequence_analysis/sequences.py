fca24d03818a712427032245690be8e89c1424d9
Krishna Kamath <krishna.kamath@gmail.com>
2011-11-14 15:04:40 -0600

    minor modifications

--- checkin_sequence_analysis/sequences.py
+++ checkin_sequence_analysis/sequences.py
@@ -5,6 +5,7 @@ Created on Nov 11, 2011
 '''
 import sys, time
 sys.path.append('../')
+from library.geo import isWithinBoundingBox
 from library.classes import GeneralMethods
 from library.file_io import FileIO
 from mongo_settings import checkinsCollection,\
@@ -13,7 +14,7 @@ from analysis.mr_analysis import filteredUserIterator
 from settings import minLocationsTheUserHasCheckedin,\
     minUniqueUsersCheckedInTheLocation, checkinSequenceGraphFile,\
     checkinSequenceLocationRegexFolder,\
-    checkinSequenceLocationRegexAnalysisFolder
+    checkinSequenceLocationRegexAnalysisFolder, us_boundary
 from operator import itemgetter
 from itertools import combinations
 import networkx as nx
@@ -165,7 +166,8 @@ class NeighboringLocationsAnalysis():
             for location, data in line['locations'].iteritems():
 #            data = line['locations']['40.735 -73.871']
 #            print line['parameters']['checkinsWindow'], [l[0][0] for l, _ in data['neigboringLocations'][:5]]
-                print line['parameters']['checkinsWindow'], location, [l[0][0] for l, _ in data['neigboringLocations'][:5]]
+                if isWithinBoundingBox(location, us_boundary):
+                    print line['parameters']['checkinsWindow'], location, [l[0][0] for l, _ in data['neigboringLocations'][:5]]
             exit()
 
 if __name__ == '__main__':
